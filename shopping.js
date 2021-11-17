a3bcbcbc4dd0f5f7c9da582512fcb0a7847147ce
cjgochanour <cjgochanour@gmail.com>
2021-11-17 13:46:39 -0600

    initial commit, finished ch7

---
+++ shopping.js
@@ -0,0 +1,22 @@
+const foods = [
+    {
+        id: 1,
+        name: "ground beef",
+        price: 8,
+        primaryMacro: "protein"
+    },
+    {
+        id: 2,
+        name: "bread",
+        price: 1,
+        primaryMacro: "carb"
+    },
+    {
+        id: 3,
+        name: "cheese",
+        price: 2.50,
+        primaryMacro: "fat"
+    }
+]
+
+console.log(foods)
